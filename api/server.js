878fbabdf74341d2a6c33f161db3e98140eb91a4
josh <joshdotjs@gmail.com>
2023-03-11 11:11:59 +0000

    working

--- api/server.js
+++ api/server.js
@@ -3,4 +3,6 @@ console.log("HELLO");
 const express = require("express");
 const server = express();
 
+server.use(express.json());
+
 module.exports = server;
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 console.log("HELLO");
+require("dotenv").config();
+require("colors");
 const server = require("./api/server");
-
-const PORT = process.env.PORT || 5000;
 
 // ==================================================
 
+server.get("/status", (req, res) => {
+  res.send("[GET] /status");
+});
+
+// ==================================================
+
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
+  console.log(
+    `*** server is running on: http://localhost:${PORT} in mode: ${process.env.NODE_ENV}`
+      .america
+  );
 });
