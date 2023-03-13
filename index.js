b4d1793521ea26dd69df79bf3f067a81a21ce4a9
josh <joshdotjs@gmail.com>
2023-03-13 12:28:23 +0000

    SQL

--- index.js
+++ index.js
@@ -35,6 +35,47 @@ server.get("/users", (req, res) => {
 
 // ==================================================
 
+// SQL
+const s3 = require("sqlite3").verbose();
+let sql;
+const fErr = (err) => {
+  if (err) return console.log(err);
+};
+
+// connect to DB
+const db = new s3.Database("./test.db", s3.OPEN_READWRITE, fErr);
+
+// // create table
+// sql = `CREATE TABLE users(id INTEGER PRIMARY KEY, first_name, last_name, username, password, email)`;
+// db.run(sql);
+
+// // drop table
+// sql = `DROP TABLE IF EXISTS users`;
+// db.run(sql);
+
+// // insert data into table
+sql = `INSERT INTO users(first_name, last_name, username, password, email) VALUES (?,?,?,?,?)`;
+// db.run(sql, ["Josh", "holloway", "joshDotJS", "1234", "josh@josh.com"], fErr);
+
+// update:
+// sql = `UPDATE users SET first_name = ? WHERE id = ?`;
+// db.run(sql, ["Steve", 1], fErr);
+
+// delete:
+// sql = `DELETE FROM users WHERE id = ?`;
+// db.run(sql, [1], fErr);
+
+// query the data
+sql = `SELECT * FROM USERS`;
+db.all(sql, [], (err, rows) => {
+  if (err) return console.log(err);
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
+
+// ==================================================
+
 server.get("/", (req, res) => {
   // res.send('<h1>fail</h1>');
   res.sendFile(path.join(__dirname, "index.html"));
