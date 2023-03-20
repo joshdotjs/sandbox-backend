f63a91e5be30ed7baf92cee0db20261fe65bf5bb
josh <joshdotjs@gmail.com>
2023-03-20 12:28:23 +0000

    controller

--- api/users/users-controller.js
+++ api/users/users-controller.js
@@ -45,6 +45,46 @@ db.all(sql, [], (err, rows) => {
 });
 
 // ==================================================
+
+// [GET] /api/users
+const getUsers = ("/", (req, res, next) => {
+  
+  // query the data
+  sql = `SELECT * FROM USERS`;
+  params = [];
+    
+  db.all(sql, params, (err, rows) => { 
+    
+    if (err) {
+      // async => use Express error-handling
+      return next({ 
+        status: 500, 
+        message: 'server error',
+        err 
+      });
+    }
+      
+    const users = [];
+    rows.forEach(row => users.push(row));
+    res.status(200).send(users);
+  });
+
+}); // server.get('/users')
+
 // ==================================================
 
-module.exports = {};
+// [POST] /api/users
+const postUser = ('/', (req, res, next) => {
+  
+  const message = '[POST] /api/user';
+  console.log(message);
+  
+  res.status(200).send({ message });
+});
+
+// ==================================================
+
+module.exports = {
+    getUsers,
+    postUser
+};
--- api/users/users-routes.js
+++ api/users/users-routes.js
@@ -2,45 +2,17 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const controller = require('./users-controller');
+const { getUsers, postUser } = require('./users-controller');
 
 // ==================================================
 
 // [GET] /api/users
-router.get("/", (req, res, next) => {
-  
-  // query the data
-  sql = `SELECT * FROM USERS`;
-  params = [];
-    
-  db.all(sql, params, (err, rows) => { 
-    
-    if (err) {
-      // async => use Express error-handling
-      return next({ 
-        status: 500, 
-        message: 'server error',
-        err 
-      });
-    }
-      
-    const users = [];
-    rows.forEach(row => users.push(row));
-    res.status(200).send(users);
-  });
-
-}); // server.get('/users')
+router.get("/", getUsers); 
 
 // ==================================================
 
 // [POST] /api/users
-router.post('/', (req, res, next) => {
-  
-  const message = '[POST] /api/user';
-  console.log(message);
-  
-  res.status(200).send({ message });
-});
+router.post('/', postUser);
 
 // ==================================================
 
