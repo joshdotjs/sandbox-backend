ed15bc47fe52d92c387d7c0c099dfd46d2b6a3bd
josh <joshdotjs@gmail.com>
2023-03-21 22:24:29 +0000

    get cars

---
+++ db/migrations/01-migration.js
@@ -0,0 +1,22 @@
+// ==================================================
+
+exports.up = async (knex) => {
+  
+  // -Drop here is already exists!
+  // await knex.schema.dropTableIfExists('cars');
+  
+  await knex.schema
+    .createTable('cars', (tbl) => {
+      tbl.increments('id');
+      tbl.string('name', 200);
+      tbl.timestamps(false, true);
+    })
+};
+
+// ==================================================
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('cars');
+};
+
+// ==================================================
