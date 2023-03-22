ec550b75eb17591ee79cc333d4be54167ffca9e1
joshdotjs <joshdotjs@gmail.com>
2023-03-22 15:22:02 -0500

    model

--- api/cars/cars-controller.js
+++ api/cars/cars-controller.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const carsModel = require('../../db/cars');
+const carsModel = require('./cars-model');
 
 // ==================================================
 
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const carsModel = require('../../db/cars');
-
 const controller = require('./cars-controller');
 
 // ==================================================
